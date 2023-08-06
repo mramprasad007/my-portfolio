43c4d8396e5da55a970041ba188e7493b63de0eb
Ram Prasad <mramprasad007@gmail.com>
2023-08-06 12:06:45 +0530

    add eslint

---
+++ src/app/app.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = (): JSX.Element => {
+  return (
+    <div className="App">
+      <header className="App-header">
+         <p>
+           Portfolio coming soon
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
