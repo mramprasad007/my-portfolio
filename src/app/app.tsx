7ae20af4192600b4fa71dc2761ca17904d6ad1b5
Ram Prasad <mramprasad007@gmail.com>
2023-08-06 15:32:24 +0530

    update scss

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import styles from './app.module.scss'
 const App = (): JSX.Element => {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className={`${styles.app}` }>
+      <header className={`${styles.app_header}   ${styles.app_header__red}`}>
          <p>
            Portfolio coming soon
         </p>
