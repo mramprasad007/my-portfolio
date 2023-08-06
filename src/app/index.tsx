1da16ee70ea273f07faa6965a10b9cf558640dec
Ram Prasad <mramprasad007@gmail.com>
2023-08-06 19:55:13 +0530

    update app

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,3 +1,15 @@
-import App from './app'
+import React from 'react'
+import styles from './app.module.scss'
+const App = (): JSX.Element => {
+  return (
+    <div>
+      <header className={`${styles.header}   ${styles.header__red}`}>
+         <p>
+           Portfolio coming soon
+        </p>
+      </header>
+    </div>
+  )
+}
 
 export default App
