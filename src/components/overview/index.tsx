43dff44922f6d2f8188591291d896eed1561b0cc
Ram Prasad <mramprasad007@gmail.com>
2023-08-15 12:28:42 +0530

    initial commit

--- src/components/overview/index.tsx
+++ src/components/overview/index.tsx
@@ -1,9 +1,41 @@
 import React from 'react'
 import styles from './overview.module.scss'
+import ramImage from '../../assets/ram-image.png'
+import downloadIcon from '../../assets/download.svg'
+import githubIcon from '../../assets/github.svg'
+import linkedinIcon from '../../assets/linkedin.svg'
 
 const Overview = (): JSX.Element => {
   return (
       <div className={`${styles.overview}`}>
+        <div className={`${styles.overview_bio}`}>
+          <div className={`${styles.overview_desc}`}>
+            <div className={`${styles.overview_hi}`}>
+              Hi, I am
+            </div>
+            <div className={`${styles.overview_name}`}>
+              Ram Prasad
+            </div>
+            <div className={`${styles.overview_tagline}`}>
+              Full-Stack / Front-End Developer
+            </div>
+            <div className={`${styles.overview_social}`}>
+              <div className={`${styles.overview_social_icon}`}>
+                <img src={downloadIcon} alt="download icon"></img>
+              </div>
+              <div className={`${styles.overview_social_icon}`}>
+              <img src={linkedinIcon} alt="linkedin icon"></img>
+                </div>
+              <div className={`${styles.overview_social_icon}`}>
+                <img src={githubIcon} alt="github icon"></img>
+              </div>
+            </div>
+          </div>
+          <div className={`${styles.overview_image}`}>
+            <img src={ramImage} alt="ram image"></img>
+          </div>
+        </div>
+        <div className={`${styles.overview_summary}`}></div>
       </div>
   )
 }
