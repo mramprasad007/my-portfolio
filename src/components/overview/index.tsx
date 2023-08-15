7f224b077a8bf7291fd8eab7b053804fc959cad7
Ram Prasad <mramprasad007@gmail.com>
2023-08-15 18:10:37 +0530

    initial commit

--- src/components/overview/index.tsx
+++ src/components/overview/index.tsx
@@ -5,7 +5,26 @@ import downloadIcon from '../../assets/download.svg'
 import githubIcon from '../../assets/github.svg'
 import linkedinIcon from '../../assets/linkedin.svg'
 
+const resumeDownloadLink = 'https://drive.google.com/uc?export=download&id=18RA4OfuTMI-MO_ml3T94ms5IM0eJH1xW'
+const linkedInLink = 'https://www.linkedin.com/in/ramprasad94'
+const githubLink = 'https://github.com/mramprasad007'
+
 const Overview = (): JSX.Element => {
+  const handleDownload = (link: string, name: string): void => {
+    // Setting various property values
+    const alink = document.createElement('a')
+    alink.href = link
+    alink.download = name
+    alink.click()
+  }
+  const handleRedirect = (link: string): void => {
+    // Setting various property values
+    const alink = document.createElement('a')
+    alink.href = link
+    alink.target = '_blank'
+    alink.rel = 'noopener noreferrer'
+    alink.click()
+  }
   return (
     <div className={`${styles.overview}`}>
       <div className={`${styles.overview_bio}`}>
@@ -21,13 +40,13 @@ const Overview = (): JSX.Element => {
               Full-Stack / Front-End Developer
             </div>
             <div className={`${styles.overview_social}`}>
-              <div className={`${styles.overview_social_icon}`}>
+              <div className={`${styles.overview_social_icon}`} onClick={() => { handleDownload(resumeDownloadLink, 'Resume.pdf') }}>
                 <img src={downloadIcon} alt="download icon"></img>
               </div>
-              <div className={`${styles.overview_social_icon}`}>
+              <div className={`${styles.overview_social_icon}`} onClick={() => { handleRedirect(linkedInLink) }}>
                 <img src={linkedinIcon} alt="linkedin icon"></img>
               </div>
-              <div className={`${styles.overview_social_icon}`}>
+              <div className={`${styles.overview_social_icon}`} onClick={() => { handleRedirect(githubLink) }}>
                 <img src={githubIcon} alt="github icon"></img>
               </div>
             </div>
